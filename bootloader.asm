ce9c79d22ba046648c3f0fbad1015db140fc3a19
Ross Archer <rarcher@enovix.com>
2024-06-24 20:36:52 -0700

    Adding comments, clean up cruft

--- bootloader.asm
+++ bootloader.asm
@@ -1,40 +1,55 @@
 ;
-; FIXME:  Lots of stye mismatch and format ugliness due to merging different code.
-;	For now, leave it be so it's obvious what is new and old code.
-;	Do a clean-up once there's some confidence in the new parts\\
-;
-;	Also, re-write code directy from WDCmon in case there are copyright issues.
-;	It's all very straightforward code because no sense in importing the nonsense.
-;
-;Intel HEX File Downloader
+; Ultra-basic bootloader.  Not yet working.
 ; R. Archer 6/2024
 ; Assembled with 64TASS
 ; 		64tass -c bootloader.asm -L bootloader.lst
+; 
+;
+; Zero page storage:
+;
+DPL 		=	$00     ; data pointer (two bytes)
+DPH  		=   $01     ; high of data pointer
+RECLEN  	=   $02     ; record length in bytes
+START_LO  	=   $03
+START_HI 	=   $04
+RECTYPE 	=   $05
+CHKSUM 		=   $06     ; record checksum accumulator
+DLFAIL		=   $07     ; flag for download failure
+TEMP 		=   $08     ; save hex value
 
-
-; TIDE2VIA	
+; TIDE2VIA	the system VIA
 ; IO for the VIA which is used for the USB debugger interface.
-SYSTEM_VIA_IORB              = $7FE0 ; Port B IO register
-SYSTEM_VIA_IORA              = $7FE1 ; Port A IO register
-SYSTEM_VIA_DDRB             = $7FE2 ; Port B data direction register
-SYSTEM_VIA_DDRA             = $7FE3 ; Port A data direction register
-SYSTEM_VIA_T1C_L           = $7FE4 ; Timer 1 counter/latches, low-order
-SYSTEM_VIA_T1C_H           = $7FE5 ; Timer 1 high-order counter
-SYSTEM_VIA_T1L_L           = $7FE6 ; Timer 1 low-order latches
-SYSTEM_VIA_T1L_H           = $7FE7 ; Timer 1 high-order latches
-SYSTEM_VIA_T2C_L           = $7FE8 ; Timer 2 counter/latches, lower-order
-SYSTEM_VIA_T2C_H           = $7FE9 ; Timer 2 high-order counter
-SYSTEM_VIA_SR              = $7FEA ; Shift register
-SYSTEM_VIA_ACR              = $7FEB ; Auxilliary control register
-SYSTEM_VIA_PCR              = $7FEC ; Peripheral control register
-SYSTEM_VIA_IFR             = $7FED ; Interrupt flag register
-SYSTEM_VIA_IER             = $7FEE ; Interrupt enable register
-SYSTEM_VIA_ORA_IRA         = $7FEF ; Port A IO register, but no handshake
-
+SYSTEM_VIA_IORB     = $7FE0 ; Port B IO register
+SYSTEM_VIA_IORA     = $7FE1 ; Port A IO register
+SYSTEM_VIA_DDRB     = $7FE2 ; Port B data direction register
+SYSTEM_VIA_DDRA     = $7FE3 ; Port A data direction register
+SYSTEM_VIA_T1C_L    = $7FE4 ; Timer 1 counter/latches, low-order
+SYSTEM_VIA_T1C_H    = $7FE5 ; Timer 1 high-order counter
+SYSTEM_VIA_T1L_L    = $7FE6 ; Timer 1 low-order latches
+SYSTEM_VIA_T1L_H    = $7FE7 ; Timer 1 high-order latches
+SYSTEM_VIA_T2C_L    = $7FE8 ; Timer 2 counter/latches, lower-order
+SYSTEM_VIA_T2C_H    = $7FE9 ; Timer 2 high-order counter
+SYSTEM_VIA_SR       = $7FEA ; Shift register
+SYSTEM_VIA_ACR      = $7FEB ; Auxilliary control register
+SYSTEM_VIA_PCR      = $7FEC ; Peripheral control register
+SYSTEM_VIA_IFR		= $7FED ; Interrupt flag register
+SYSTEM_VIA_IER      = $7FEE ; Interrupt enable register
+SYSTEM_VIA_ORA_IRA	= $7FEF ; Port A IO register, but no handshake
+; System VIA named bitmasks
+PB0 = MASK0
+PB1 = MASK1
+PB2 = MASK2
+PB3 = MASK3
+PB5 = MASK5
+FIFO_TXE = PB0
+FIFO_RXF = PB1
+FIFO_WR = PB2
+FIFO_RD = PB3
+FIFO_PWREN = PB5
 
 ; 6551 ACIA equates for serial I/O
 ;
-ACIA_BASE= $7F30	; This is where the 6551 ACIA starts
+ACIA_BASE= $7F30		; This is where the 6551 ACIA starts
 SDR = ACIA_BASE       	; RX'ed bytes read, TX bytes written, here
 SSR = ACIA_BASE+1     	; Serial data status register. A write here
                                 ; causes a programmed reset.
@@ -49,16 +64,6 @@ SCMD_V  = %00001011       ; No parity, no echo, no tx or rx IRQ, DTR*
 TX_RDY  = %00010000       ; AND mask for transmitter ready
 RX_RDY  = %00001000       ; AND mask for receiver buffer full
 ;
-; Zero-page storage
-DPL =            $00     ; data pointer (two bytes)
-DPH  =           $01     ; high of data pointer
-RECLEN  =        $02     ; record length in bytes
-START_LO  =      $03
-START_HI =       $04
-RECTYPE =        $05
-CHKSUM =         $06     ; record checksum accumulator
-DLFAIL=          $07     ; flag for download failure
-TEMP =          $08     ; save hex value
 
 ; "Shadow" RAM vectors (note each is $8000 below the actual ROM vector)
 NMIVEC	= 	 $7EFA	; write actual NMI vector here
@@ -74,51 +79,25 @@ MASK5	=	 %00100000
 MASK6	=	 %01000000
 MASK7	=	 %10000000
 
-
-ENTRY_POINT = 	$2000	; where the RAM program MUST have its first instruction
-*= $F800
-START   sei                     ; disable interrupts
-        cld                     ; binary mode arithmetic
-        ldx     #$FF            ; Set up the stack pointer
-        txs                     ;       "
+*= $F800						; Monitor start address
+START   SEI                     ; disable interrupts
+        ; CLD                     ; binary mode arithmetic (not required on 65C02 or 65816)
+        LDX     #$FF            ; Set up the stack pointer
+        TXS                     ;       "
         LDA     #>START      	; Initialiaze the interrupt vectors
-        sta     NMIVEC+1        ; User program at ENTRY_POINT may change
-        sta     IRQVEC+1	; these vectors.  Just do change before enabling
+        STA     NMIVEC+1        ; User program at ENTRY_POINT may change
+        STA     IRQVEC+1	; these vectors.  Just do change before enabling
         LDA     #<START		; the interrupts, or you'll end up back in the d/l monitor.
-        sta     NMIVEC
-        sta     IRQVEC
-	jsr	INITVIA		; Set up 65C22 to FIFO interface chip (and ROM bank select)
+        STA     NMIVEC
+        STA     IRQVEC
+		JSR		INITVIA		; Set up 65C22 to FIFO interface chip (and ROM bank select)
+ECHO	LDA		#'*'
+		JSR		FIFOOUT		; Just send something in case FIFOIN hangs so we know we got this far FIXME: remove this
+ECHO2	JSR		FIFOIN
+		BCC		ECHO2		; Wait for an incoming character
+		JSR		FIFOOUT
+		BRA		ECHO2
 
-ECHO
-	jsr	INFIFO
-	pha
-	ldy 	#$10
-	jsr	WASTETM
-	pla
-	jsr	OUTFIFO
-	bra	ECHO
-
-	       
-SPAMLOOP
-	sta	$4000		; ram for CS LED side effect
-	lda 	#'*'
-	jsr 	OUTFIFO
-	LDY	#$80
-	jsr	WASTETM
-	bra	SPAMLOOP
-
-; System VIA named bitmasks
-PB0 = MASK0
-PB1 = MASK1
-PB2 = MASK2
-PB3 = MASK3
-PB5 = MASK5
-
-FIFO_TXE = PB0
-FIFO_RXF = PB1
-FIFO_WR = PB2
-FIFO_RD = PB3
-FIFO_PWREN = PB5
 
 ;;;; ============================= New FIFO functions ======================================
 ; Initializes the system VIA (the USB debugger), and syncs with the USB chip.
